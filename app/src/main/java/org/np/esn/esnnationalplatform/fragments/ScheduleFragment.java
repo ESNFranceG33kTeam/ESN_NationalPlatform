6bb5aeeb89924cdce9c62760b86b62f463a49362
Madalina Diaconu <maddaaa04@yahoo.com>
2017-02-15 23:28:45 +0100

    Implement Schedule as a WebView

--- app/src/main/java/org/np/esn/esnnationalplatform/fragments/ScheduleFragment.java
+++ app/src/main/java/org/np/esn/esnnationalplatform/fragments/ScheduleFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import org.np.esn.esnnationalplatform.R;
 
@@ -14,7 +16,12 @@ public class ScheduleFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_schedule, container, false);
+        WebView webView = (WebView) rootView.findViewById(R.id.events_calendar);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://calendar.google.com/calendar/embed?src=spcepnmj0t34lp6ujgp8rdqbus@group.calendar.google.com&ctz=Europe/Brussels&pli=1");
+        return rootView;
     }
 
     @Override
